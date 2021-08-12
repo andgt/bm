70a75ed6ddaf73877136a9e2e5a46e7513da6bdc
anatoliy <anatolijdegtyar@yandex.ru>
2021-08-12 18:09:54 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -48,6 +48,7 @@ let tab = function() {
     tabBtn.forEach(element => {
       element.classList.remove("button__tab--active");
     });
+
     this.classList.add("button__tab--active");
     tabName = this.getAttribute("data-tab");
     selectTabContent(tabName);
@@ -65,3 +66,58 @@ let tab = function() {
 };
 
 tab();
+
+let tabInner = function() {
+
+  function selectTab(evt) {
+
+    this.classList.add("button__tab--active");
+    tabName = this.getAttribute("data-tab");
+    selectTabContent(tabName);
+  }
+
+  function selectTabContent(tabName) {
+    cardsBlock.forEach(element => {
+      if (element.classList.contains(tabName)) {
+        element.classList.add("objects__card-slider--active");
+      } else {
+        element.classList.remove("objects__card-slider--active");
+      }
+    });
+  };
+};
+
+let tabLink = function() {
+  let tabLinkBtn = document.querySelectorAll(".about__link");
+  let cardsLinkBlock = document.querySelectorAll(".button__tab");
+  let tabLinkName;
+  const el = document.getElementById('objects');
+
+
+  tabLinkBtn.forEach(element => {
+    element.addEventListener("click", selectLinkTab)
+  });
+
+  function selectLinkTab(evt) {
+    evt.preventDefault();
+    el.scrollIntoView();
+    tabLinkName = this.getAttribute("data-tab-objects");
+    selectTabContent(tabLinkName);
+  }
+
+  function selectTabContent(tabLinkName) {
+    cardsLinkBlock.forEach(element => {
+      if (element.classList.contains(tabLinkName)) {
+        element.classList.add("button__tab--active");
+      } else {
+        element.classList.remove("button__tab--active");
+      }
+    });
+  };
+};
+
+tabLink();
+
+window.onscroll = function() {
+  menuFixed();
+};
